75cc96ea8e5f2ef2bc692439b33df14c8a5d5cdf
yanghu <410447115@qq.com>
2020-09-22 11:39:04 +0800

    modify

--- Bos_MyUtils/src/main/java/com/yanghu/utils/SmsUtils.java
+++ Bos_MyUtils/src/main/java/com/yanghu/utils/SmsUtils.java
@@ -37,8 +37,8 @@ public class SmsUtils {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIkixA1UE9KyeU";
-    static final String accessKeySecret = "gfZSin9FIPW4UEda5g8AmHAJQlowQI";
+    static final String accessKeyId = "xxx";
+    static final String accessKeySecret = "zzz";
 
     public static SendSmsResponse sendSms(String telephone,String code) throws ClientException {
 
